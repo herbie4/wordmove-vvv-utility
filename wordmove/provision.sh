c8a16a4cd1b2c902c690f9efcf1f9556fd3ec944
haha.nl - herbert hoekstra <werk@haha.nl>
2020-04-23 17:22:32 +0200

    added sudo on command

--- wordmove/provision.sh
+++ wordmove/provision.sh
@@ -2,8 +2,6 @@
 
 # Rubygems update
 
-sudo -i
-
 if [ $(gem -v|grep '^2.') ]; then
 	echo "gem installed"
 else
@@ -20,7 +18,7 @@ if [ "$wordmove_install" = true ]; then
   echo "wordmove installed"
 else
   echo "wordmove not installed"
-  gem install wordmove
+  sudo gem install wordmove
 
   wordmove_path="$(gem which wordmove | sed -s 's/.rb/\/deployer\/base.rb/')"
   if [  "$(grep yaml $wordmove_path)" ]; then
